a7a745fbf4d0f6d6f99bb2c8fb5b19fe172894f9
Paddy Gallagher <paddy.gallagher@gmail.com>
2020-10-25 12:23:47 +0000

    Add a todo

---
+++ app.js
@@ -0,0 +1,35 @@
+// This is the array that will hold the todo list items
+let todoItems = [];
+
+// This function will create a new todo object based on the
+// text that was entered in the text input, and push it into
+// the `todoItems` array
+function addTodo(text) {
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now(),
+  };
+
+  todoItems.push(todo);
+  console.log(todoItems);
+}
+
+// Select the form element
+const form = document.querySelector('.js-form');
+
+// Add a submit event listener
+form.addEventListener('submit', (event) => {
+  // prevent page refresh on form submission
+  event.preventDefault();
+  // select the text input
+  const input = document.querySelector('.js-todo-input');
+
+  // Get the value of the input and remove whitespace
+  const text = input.value.trim();
+  if (text !== '') {
+    addTodo(text);
+    input.value = '';
+    input.focus();
+  }
+});
