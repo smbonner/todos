20d1b5a3df56b3ea079fc0704b885371138acfb8
Seamus Bonner <smbonner@seamus-bonners-imac-2.local>
2020-10-31 10:58:10 +0000

    Render todos

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 // This is the array that will hold the todo list items
 let todoItems = [];
 
+function renderTodo(todo) {
+  // Select the first element with a class of `js-todo-list`
+  const list = document.querySelector('.js-todo-list');
+
+  // Use the ternary operator to check if `todo.checked` is true
+  // if so, assign 'done' to `isChecked`. Otherwise, assign an empty string
+  const isChecked = todo.checked ? 'done': '';
+  // Create an `li` element and assign it to `node`
+  const node = document.createElement("li");
+  // Set the class attribute
+  node.setAttribute('class', `todo-item ${isChecked}`);
+  // Set the data-key attribute to the id of the todo
+  node.setAttribute('data-key', todo.id);
+  // Set the contents of the `li` element created above
+  node.innerHTML = `
+    <input id="${todo.id}" type="checkbox"/>
+    <label for="${todo.id}" class="tick js-tick"></label>
+    <span>${todo.text}</span>
+    <button class="delete-todo js-delete-todo">
+    <svg><use href="#delete-icon"></use></svg>
+    </button>
+  `;
+
+  // Append the element to the DOM as the last child of
+  // the element referenced by the `list` variable
+  list.append(node);
+}
+
 // This function will create a new todo object based on the
 // text that was entered in the text input, and push it into
 // the `todoItems` array
@@ -12,7 +40,7 @@ function addTodo(text) {
   };
 
   todoItems.push(todo);
-  console.log(todoItems);
+  renderTodo(todo);
 }
 
 // Select the form element
